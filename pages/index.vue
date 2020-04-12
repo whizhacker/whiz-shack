94d9f351e6ad6f533ae6c692d5bc8e9ac1ebce2d
Dimas Victor <quetzalcroix@gmail.com>
2020-04-13 06:44:07 +0700

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -11,9 +11,7 @@
         </v-card-title>
         <v-card-text>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
+            This is my personal Blog
           </p>
           <p>
             For more information on Vuetify, check out the
